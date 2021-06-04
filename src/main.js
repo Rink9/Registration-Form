1cc00af8b6388a2da0aba62be8bbbefc0f1400ce
Rinky Chowdhury <rink.chowdhury1559@gmail.com>
2021-06-04 13:45:17 +0600

    Updated background color

--- src/main.js
+++ src/main.js
@@ -1,17 +1,21 @@
 function createAccount() {
     let inputs = document.getElementsByClassName("formField");
-    let errorMessege ;
+    let body = document.getElementsByTagName("body");
+    let errorMessege;
+
 
     for (let i = 0; i < inputs.length; i++) {
         let value = inputs[i].value;
         if (!value) {
             errorMessege = document.getElementsByClassName("formField")
             inputs[i].style.backgroundColor = "#FEE2E2";
-            let body = document.getElementsByTagName("body");
             body[0].style.backgroundColor = "#FEE2E2";
             let btn = document.getElementById("accountCreated");
             btn.style.backgroundColor = "#FEE2E2";
+        } else {
+            inputs[i].style.backgroundColor = "#FFFF";
         }
+
 
     }
 
