ddabc24b6f7197603402b12147d7a7323c1f57d0
Rinky Chowdhury <rink.chowdhury1559@gmail.com>
2021-06-04 13:27:31 +0600

    Updated JS when field are empty

--- src/main.js
+++ src/main.js
@@ -1,15 +1,19 @@
 function createAccount() {
     let inputs = document.getElementsByClassName("formField");
-    let errorMessege = "";
+    let errorMessege ;
 
-    for(let i = 0; i < inputs.length; i++) {
+    for (let i = 0; i < inputs.length; i++) {
         let value = inputs[i].value;
-        if (!value){
+        if (!value) {
             errorMessege = document.getElementsByClassName("formField")
-            formField[i].style.backgroundColor = "red";
+            inputs[i].style.backgroundColor = "#FEE2E2";
+            let body = document.getElementsByTagName("body");
+            body[0].style.backgroundColor = "#FEE2E2";
+            let btn = document.getElementById("accountCreated");
+            btn.style.backgroundColor = "#FEE2E2";
         }
 
-    } 
+    }
 
     if (!errorMessege) {
         accountCreated();
